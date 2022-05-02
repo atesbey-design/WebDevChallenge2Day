0a3a62ccc65f276d33721347e7aca7ee3e717043
atesbey-design <tuncerbostancibasi@gmail.com>
2022-05-02 21:57:01 +0300

    Swap Page

---
+++ active.js
@@ -0,0 +1,24 @@
+function clicked(){
+
+	alert("İstenilen sayfaya şuanda ulaşılamıyor.");
+}
+function transfer(){
+	
+	let current1= document.getElementById("current1");
+	let current2=document.getElementById("current2");
+	
+	let storage=current1.innerHTML;
+	
+	current1.innerHTML=current2.innerHTML;
+	current2.innerHTML=storage;
+
+	
+	
+	
+}
+
+function changeBackground(){
+	let background= document.getElementById("column1");
+	
+	background.className+="changeBackground";
+}
